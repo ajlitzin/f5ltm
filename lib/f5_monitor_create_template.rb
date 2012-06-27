22aadeab2d670ec4f4381f82c599fac0cf627c89
Andy Litzinger <andy.litzinger@gmail.com>
2012-06-27 14:45:01 -0700

    f5_monitor_create.rb now properly uses command line options when populating hashes

--- lib/f5_monitor_create_template.rb
+++ lib/f5_monitor_create_template.rb
@@ -13,8 +13,14 @@ class Optparser
       opts.banner = "Usage: f5_monitor_create_template.rb [options]"
       opts.separator ""
       opts.separator "Specific options:"
-      options.type = "TTYPE_HTTP"
       
+      options.type = "TTYPE_HTTP"
+      options.interval = 5
+      options.timeout = 16
+      options.monitor_ip = "0.0.0.0"
+      options.monitor_port = "0"
+      options.address_type = "ATYPE_STAR_ADDRESS_STAR_PORT"
+      options.parent_template = "http"
            
       opts.on( "-b", "--bigip IP", "BigIP IP address") do |bip|
         options.bigip = bip
@@ -25,23 +31,25 @@ class Optparser
       opts.on("--type [TEMPLATE TYPE]", "Template Type") do |type|
         options.template_type = type || "TTYPE_HTTP"
       end
-      opts.on("--parent_template [PARENT TEMPLATE]", "Parent Template to inherit from") do |parent|
-	    options.parent_template = parent
+      opts.on("--parent_template PARENT TEMPLATE", "Parent Template to inherit from") do |parent|
+	      options.parent_template = parent || "http"
       end
 	    opts.on("--interval [INTERVAL]", "How frequently the monitor instance of this template will run") do |int|
-	      options.interval = int
+	      options.interval = int || 5
       end
 	    opts.on("--timeout [TIMEOUT]", "The Number of seconds in which the node or service must respond to the monitor request") do |timeout|
 	      options.timeout = timeout
       end
-	    ## FIX THIS- SPLIT IT UP
+	    
 	    opts.on("--monitor_ip [MonitorIP]", "The destination IP of this monitor template") do |ip|
-	      options.monitor_ip = ip || "*"
+	      options.monitor_ip = ip || "0.0.0.0"
       end
       opts.on("--monitor_port [MonitorPort]", "The destination Port of this monitor template") do |port|
-	      options.monitor_port = port || "*"
+	      options.monitor_port = port || "0"
       end
-      
+      opts.on("--address_type [AddressType]", "Defines Node Definition, eg. ATYPE_STAR_ADDRESS_STAR_PORT") do |atype|
+	      options.address_type = atype || "ATYPE_STAR_ADDRESS_STAR_PORT"
+      end
       opts.on_tail("-h", "--help", "Show this message") do
         puts opts
         exit
@@ -65,9 +73,6 @@ end
 
 
 def create_monitor_template(lb,my_mon_template_list, my_mon_attr_list)
-pp "mon_template_list #{my_mon_template_list}"
-pp " mon attr list: #{my_mon_attr_list}"
-#debugger
   lb.icontrol.locallb.monitor.create_template(my_mon_template_list, my_mon_attr_list)
 end
 # get command line options
@@ -86,10 +91,8 @@ lb = F5::LoadBalancer.new(options.bigip, :config_file =>'../fixtures/config-andy
 
 my_mon_template = MonitorTemplate.new(options.name, options.type)
 my_mon_template_list = [my_mon_template.to_hash]
-my_dest_ipport = { 'address_type' => "ATYPE_STAR_ADDRESS_STAR_PORT", 'ipport' => {'address' => "0.0.0.0", 'port' => 0}}
-my_mon_common_attr = MonitorCommonAttributes.new("http", 5, 16, my_dest_ipport, "false", "true")
+my_dest_ipport = { 'address_type' => options.address_type, 'ipport' => {'address' => options.monitor_ip, 'port' => options.monitor_port}}
+my_mon_common_attr = MonitorCommonAttributes.new(options.parent_template, options.interval, options.timeout, my_dest_ipport, "false", "true")
 my_mon_common_attr_list = [my_mon_common_attr.to_hash]
-pp "mon template: #{my_mon_template_list}"
-pp "mon attrs: #{my_mon_common_attr_list}"
 
 create_monitor_template(lb,my_mon_template_list,my_mon_common_attr_list)
