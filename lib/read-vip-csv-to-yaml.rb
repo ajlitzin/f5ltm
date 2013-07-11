f795bb5be2d2da950119190ed687d1d0b66f0b13
Andy Litzinger <andy.litzinger@gmail.com>
2013-07-11 14:52:59 -0700

    working output to yaml that vs_builder can read in and use

--- lib/read-vip-csv-to-yaml.rb
+++ lib/read-vip-csv-to-yaml.rb
@@ -1,16 +1,28 @@
 require 'yaml'
 
+# fqdn, pool port, vip port 1, vip port 2
 inputarray = [ "andy.test.theplatform.com", 14042, 80, 443 ]
 
-main_hash = { :main => {:fqdn=> "andy.test.corp.theplatform.com", :vip_type =>'web'}}
-monitor_hash = { :monitor => { :name => "", :type => "http", :send=> " GET /management/alive", :recv => "Web Service is Ok"}}
+main_hash1 = { "fqdn"=> "andy.test.corp.theplatform.com", "vip_type" =>'web'}
 
-pool_hash = { :pool => { :name => "", :port=> 4567, :lb_method => "least_connections", :monitor_name => "", :min_active_members => 0, :action_on_service_down=> "SERVICE_DOWN_ACTION_NONE", :pool_members => { :memberdef => "", :priority=> 2}}}
+main_hash2 = { "fqdn"=> "andy.test.corp.theplatform.com2", "vip_type" =>'web'}
 
-vs_hash = { :virtual_server => { :name=> "", :address=>"1.2.3.4", :port => 80, :protocol=> "", :netmask => "", :resource_type=> "", :default_pool_name=> "", :profile_context=>"", :profile_name=>"http", :snat=> "", :mirrored_state=> "", :vlan_name=>"", :ssl_client_profile=>""} }
+monitor_hash1 = { "name" => "", "type" => "http", "send"=> " GET /management/alive", "recv" => "Web Service is Ok"}
 
-top_hash = {:service1 => [main_hash, monitor_hash, pool_hash, vs_hash]}
+pool_hash1 = { "name" => "", "port"=> 4567, "lb_method" => "least_connections", "monitor_name" => "", "min_active_members" => 0, "action_on_service_down"=> "SERVICE_DOWN_ACTION_NONE", "pool_members" => [{ "memberdef" => "192.168.244.250:8080", "priority"=> 2}, { "memberdef" => "192.168.244.251:8080", "priority"=>2 }]}
 
-puts top_hash.to_yaml
+vs_hash1 = { "name"=> "", "address"=>"1.2.3.4", "port" => 80, "protocol"=> "", "netmask" => "", "resource_type"=> "", "default_pool_name"=> "", "profile_context"=>"", "profile_name"=>"http", "snat"=> "", "mirrored_state"=> "", "vlan_name"=>"", "ssl_client_profile"=>""}
 
-File.open("../private-fixtures/andy-csv-out.yml", 'w') {|f| f.write(top_hash.to_yaml)}
+service1hash = {"service1" => { "main"=> main_hash1, "monitor"=>monitor_hash1, "pool"=> pool_hash1, "virtual_server"=> vs_hash1 }}
+service2hash = {"service2" => { "main"=> main_hash1, "monitor"=>monitor_hash1, "pool"=> pool_hash1, "virtual_server"=> vs_hash1 }}
+top_array = [service1hash,service2hash]
+
+top_array.each do |cur_hash|
+  puts cur_hash.to_yaml
+end
+
+File.open("../private-fixtures/andy-csv-out2.yml", 'w') do |file|
+  top_array.each do |cur_hash|
+    file.write(cur_hash.to_yaml)
+  end
+end
